085c02db0f56eeb8e59ff2abe3a0792e33f6db58
rodwanz <wanzervai@gmail.com>
2024-02-05 19:31:46 -0300

    Ajuste na configuração do Projeto

---
+++ src/main/java/br/wanzeler/desafio/mercadolivre/MercadoLivreApplication.java
@@ -0,0 +1,14 @@
+package br.wanzeler.desafio.mercadolivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoLivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoLivreApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/br/wanzeler/desafio/mercadolivre/MercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.wanzeler.desafio.mercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
